3018ad6a4dd8d1fc0fe7ab30c091b7f68ee61840
helenryao <helenroseyao@gmail.com>
2023-10-21 15:55:33 -0400

    changed name back, kept app_name as hoosThere

--- cs3240/settings.py
+++ cs3240/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'hoosThere',
+    'cs3240',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
