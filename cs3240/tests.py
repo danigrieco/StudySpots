3115ee529d8d2af9d08384f6a78c792fc2e5b56c
griec <tgh8wp@virginia.edu>
2023-10-29 18:05:27 -0400

    changes

---
+++ cs3240/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class ProfileViewTests(TestCase):
+
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret1234'
+        }
+        self.user = User.objects.create_user(**self.credentials)
+    def test_wrong_login_credentials(self):
+        login_status = self.client.login(username='testuser', password='wrongpassword')
+        self.assertFalse(login_status)
+
+    def test_correct_login_credentials(self):
+        login_status = self.client.login(**self.credentials)
+        self.assertTrue(login_status)
